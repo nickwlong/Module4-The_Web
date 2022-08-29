b741d3c50405f4376ee5f090ede9bb94d3dde909
nickwlong <nickwlong@gmail.com>
2022-08-29 13:11:27 +0100

    Ready for test-driving a route

--- Phase_two/app.rb
+++ Phase_two/app.rb
@@ -8,31 +8,4 @@ class Application < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  get '/hello' do
-    name = params[:name]
-    return "Hello #{name}"
-  end 
-
-  post '/submit' do
-    name = params[:name]
-    message = params[:message]
-
-    return "Thanks #{name}, you sent this message: \"#{message}\""
-  end
-
-  post '/sort-names' do
-    names = params[:names]
-    return names.split(',').sort.join(',')
-  end
-
-
-#   # Request:
-# POST /submit
-
-# # With body parameters:
-# name=Leo
-# message=Hello world
-
-# # Expected response (2OO OK):
-# Thanks Leo, you sent this message: "Hello world"
 end
